69cd64b271a296529d9b526639dcd89e772c6d82
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-25 22:57:34 +0300

    AppModule and ViewModelFactoryModule

---
+++ app/src/main/java/com/example/blogposts/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.blogposts.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://open-api.xyz/api/"
+    }
+}
---
+++ app/src/main/java/com/example/blogposts/utils/PreferenceKeys.kt
@@ -0,0 +1,10 @@
+package com.example.blogposts.utils
+
+class PreferenceKeys {
+    companion object {
+        const val APP_PREFERENCES: String = "APP_PREFERENCES"
+        const val PREVIOUS_AUTH_USER: String = "PREVIOUS_AUTH_USER"
+        const val BLOG_FILTER: String = "BLOG_FILTER"
+        const val BLOG_ORDER: String = "BLOG_ORDER"
+    }
+}
