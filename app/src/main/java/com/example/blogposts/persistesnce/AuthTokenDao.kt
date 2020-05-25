2d3f9b448b66d5aa0eb4c2523e2c40eff302ad82
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-25 21:52:26 +0300

    Room Persistence Setup

--- app/src/main/java/com/example/blogposts/persistesnce/AuthTokenDao.kt
+++ app/src/main/java/com/example/blogposts/persistesnce/AuthTokenDao.kt
@@ -1,4 +1,16 @@
 package com.example.blogposts.persistesnce
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.blogposts.models.AuthToken
+
+@Dao
 interface AuthTokenDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(authToken: AuthToken): Long
+
+    @Query("UPDATE auth_token SET token=null WHERE account_pk = :pk")
+    fun nullifyToken(pk: Int): Int
 }
