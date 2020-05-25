14e546fcae13818bba4331a63e579c00fff17750
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-25 15:42:59 +0300

    Navigation Graph for Authentication

---
+++ app/src/main/java/com/example/blogposts/ui/auth/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.blogposts.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.blogposts.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login,container,false)
+    }
+
+}
