d9e61685494c1508edc1cdcd84cc21d24407952f
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-26 23:46:47 +0300

    BlogPostAuthService

--- app/src/main/java/com/example/blogposts/api/auth/BlogPostsAuthService.kt
+++ app/src/main/java/com/example/blogposts/api/auth/BlogPostsAuthService.kt
@@ -1,5 +1,29 @@
 package com.example.blogposts.api.auth
 
+import androidx.lifecycle.LiveData
+import com.example.blogposts.api.auth.network_responses.LoginResponse
+import com.example.blogposts.api.auth.network_responses.RegistrationResponse
+import com.example.blogposts.utils.GenericApiResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface BlogPostsAuthService {
+
+    @POST
+    @FormUrlEncoded
+    fun login(
+        @Field("username") email: String,
+        @Field("password") password: String
+    ): LiveData<GenericApiResponse<LoginResponse>>
+
+    @POST
+    @FormUrlEncoded
+    fun register(
+        @Field("email") email: String,
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Field("password2") password2: String
+    ): LiveData<GenericApiResponse<RegistrationResponse>>
 
 }
