c8706047bd0466dbcf196ec36e03e2985f51a23b
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-27 21:10:32 +0300

    Login Fragment with viewModel subscribe

--- app/src/main/java/com/example/blogposts/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/blogposts/ui/auth/LoginFragment.kt
@@ -8,9 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import com.example.blogposts.R
-import com.example.blogposts.utils.ApiEmptyResponse
-import com.example.blogposts.utils.ApiErrorResponse
-import com.example.blogposts.utils.ApiSuccessResponse
+import com.example.blogposts.ui.auth.state.LoginFields
+
+import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * A simple [Fragment] subclass.
@@ -28,22 +28,27 @@ class LoginFragment : BaseAuthFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG, "LoginFragment: ${viewModel.hashCode()}")
-        viewModel.testLogin().observe(viewLifecycleOwner, Observer { response ->
-            when (response) {
-                is ApiSuccessResponse -> {
-                    Log.d(TAG, "LOGIN RESPONSE: ${response.body}")
-                }
+        subscribeObservers()
+    }
 
-                is ApiErrorResponse -> {
-                    Log.d(TAG, "LOGIN RESPONSE: ${response.errorMessage}")
-                }
-
-                is ApiEmptyResponse -> {
-                    Log.d(TAG, "LOGIN RESPONSE: EMPTY RESPONSE")
-
+    fun subscribeObservers() {
+        viewModel.viewState.observe(viewLifecycleOwner, Observer {
+            it.loginFields?.let { loginFields ->
+                loginFields.login_email?.let { email ->
+                    input_email.setText(email)
                 }
             }
         })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewModel.setLoginFields(
+            LoginFields(
+                input_email.text.toString(),
+                input_password.text.toString()
+            )
+        )
     }
 
 }
