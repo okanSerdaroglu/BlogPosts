74ea9a629cbce9fc179518999fa1c4b47fd603f6
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-12 15:42:05 +0300

    TODO Removed

--- app/src/main/java/com/example/blogposts/di/AppInjector.kt
+++ app/src/main/java/com/example/blogposts/di/AppInjector.kt
@@ -17,33 +17,21 @@ object AppInjector {
             .build().inject(app)
 
         app.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-            override fun onActivityPaused(activity: Activity) {
-                TODO("Not yet implemented")
-            }
+            override fun onActivityPaused(activity: Activity) {}
 
-            override fun onActivityStarted(activity: Activity) {
-                TODO("Not yet implemented")
-            }
+            override fun onActivityStarted(activity: Activity) {}
 
-            override fun onActivityDestroyed(activity: Activity) {
-                TODO("Not yet implemented")
-            }
+            override fun onActivityDestroyed(activity: Activity) {}
 
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-                TODO("Not yet implemented")
-            }
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
 
-            override fun onActivityStopped(activity: Activity) {
-                TODO("Not yet implemented")
-            }
+            override fun onActivityStopped(activity: Activity) {}
 
             override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
                 handleActivity(activity)
             }
 
-            override fun onActivityResumed(activity: Activity) {
-                TODO("Not yet implemented")
-            }
+            override fun onActivityResumed(activity: Activity) {}
 
         })
     }
