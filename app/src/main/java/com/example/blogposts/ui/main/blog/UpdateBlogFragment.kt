b5ca27de54ca197d96c030f3dc157236f166ef7a
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-12 11:15:57 +0300

    Updating a BlogPost Image

--- app/src/main/java/com/example/blogposts/ui/main/blog/UpdateBlogFragment.kt
+++ app/src/main/java/com/example/blogposts/ui/main/blog/UpdateBlogFragment.kt
@@ -1,16 +1,31 @@
 package com.example.blogposts.ui.main.blog
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.*
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.blogposts.R
+import com.example.blogposts.ui.*
 import com.example.blogposts.ui.main.blog.state.BlogStateEvent
 import com.example.blogposts.ui.main.blog.viewmodel.onBlogPostUpdateSuccess
 import com.example.blogposts.ui.main.blog.viewmodel.setUpdatedBlogFields
-import kotlinx.android.synthetic.main.fragment_view_blog.*
+import com.example.blogposts.utils.Constants
+import com.example.blogposts.utils.ErrorHandling
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
+import kotlinx.android.synthetic.main.fragment_update_blog.*
+import kotlinx.android.synthetic.main.fragment_view_blog.blog_body
+import kotlinx.android.synthetic.main.fragment_view_blog.blog_image
+import kotlinx.android.synthetic.main.fragment_view_blog.blog_title
+import okhttp3.MediaType
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
 
 class UpdateBlogFragment : BaseBlogFragment() {
 
@@ -27,6 +42,12 @@ class UpdateBlogFragment : BaseBlogFragment() {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
         subscribeObservers()
+
+        image_container.setOnClickListener {
+            if (stateChangeListener.isStoragePermissionGranted()) {
+                pickFromGallery()
+            }
+        }
     }
 
     private fun subscribeObservers() {
@@ -74,16 +95,102 @@ class UpdateBlogFragment : BaseBlogFragment() {
         inflater.inflate(R.menu.update_menu, menu)
     }
 
+    private fun pickFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        val mimeTypes = arrayOf("image/jpeg", "image/png", "image/jpg")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        startActivityForResult(intent, Constants.GALLERY_REQUEST_CODE)
+    }
+
+    private fun launchImageCrop(uri: Uri?) {
+        context?.let {
+            CropImage.activity(uri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(it, this)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                Constants.GALLERY_REQUEST_CODE -> {
+                    data?.data?.let { uri ->
+                        launchImageCrop(uri)
+                    } ?: showErrorDialog(ErrorHandling.ERROR_SOMETHING_WRONG_WITH_IMAGE)
+                }
+
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                    Log.d(TAG, "CROP: CROP_IMAGE_ACTIVITY_REQUEST_CODE")
+                    val result = CropImage.getActivityResult(data)
+                    val resultUri = result.uri
+                    Log.d(TAG, "CROP: CROP_IMAGE_ACTIVITY_REQUEST_CODE: uri: $resultUri")
+                    viewModel.setUpdatedBlogFields(
+                        title = null,
+                        body = null,
+                        uri = resultUri
+                    )
+                }
+
+                CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE -> {
+                    showErrorDialog(ErrorHandling.ERROR_SOMETHING_WRONG_WITH_IMAGE)
+                }
+            }
+        }
+    }
+
     private fun saveChanges() {
         var multipartBody: MultipartBody.Part? = null
-        viewModel.setStateEvent(
-            BlogStateEvent.UpdatedBlogPostEvent(
-                title = blog_title.text.toString(),
-                body = blog_body.text.toString(),
-                image = multipartBody
+        viewModel.viewState.value?.updateBlogFields?.updatedImageUri?.let { imageUri ->
+            imageUri.path?.let { filePath ->
+                val imageFile = File(filePath)
+                Log.d(TAG, "UpdateBlogFragment:imageFile: $imageFile")
+                val requestBody = RequestBody.create(
+                    MediaType.parse("image/*"),
+                    imageFile
+                )
+                // name = field name in serializer
+                // filename = name of image file
+                // requestBody = file with file type information
+                multipartBody = MultipartBody.Part.createFormData(
+                    "image",
+                    imageFile.name,
+                    requestBody
+                )
+            }
+        }
+
+        multipartBody?.let {
+            viewModel.setStateEvent(
+                BlogStateEvent.UpdatedBlogPostEvent(
+                    blog_title.text.toString(),
+                    blog_body.text.toString(),
+                    it
+                )
+            )
+            stateChangeListener.hideSoftKeyboard()
+        } ?: showErrorDialog(ErrorHandling.ERROR_MUST_SELECT_IMAGE)
+    }
+
+    private fun showErrorDialog(errorMessage: String) {
+        stateChangeListener.onDataStateChange(
+            DataState(
+                error = Event(
+                    content = StateError(
+                        response = Response(
+                            message = errorMessage,
+                            responseType = ResponseType.Dialog()
+                        )
+                    )
+                ),
+                loading = Loading(isLoading = false),
+                data = Data(data = Event.dataEvent(null), response = null)
             )
         )
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
